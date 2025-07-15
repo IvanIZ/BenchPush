ab40a27e1df3cdca5b60e32fa7a96db2a1b98e67
Rishi Bhatnagar <bhatnagarri22@gmail.com>
2025-07-15 14:14:07 -0400

    Area clearing code better

--- scripts/configure_area_clearing_mujoco.py
+++ scripts/configure_area_clearing_mujoco.py
@@ -6,19 +6,8 @@ import gymnasium as gym
 import random
 import numpy as np
 
-
-############### VVVVVVV Configurable Parameters for Ship-Ice VVVVVVV ####################
-cfg = {
-    "output_dir": "logs/",      # Specify directory for loggings
-    "egocentric_obs": True,     # True egocentric observation, False for global observation
-    "concentration": 0.1,       # Ice field concentration, options are 0.1, 0.2, 0.3, 0.4, 0.5
-    "goal_y": 19,                # Initial distance from the goal line
-    "render_scale": 30,         # Scalar applied to rendering window to fit the screen. Reducing this value makes rendering window smaller
-}
-############### ^^^^^^^ Configurable Parameters for Ship-Ice ^^^^^^^ ####################
-
-
 env = gym.make('area-clearing-mujoco-v0', render_mode = "human", cfg=cfg)
+env.reset()
 
 terminated = truncated = False
 num_epochs = 20
@@ -29,7 +18,6 @@ for i in range(num_epochs):
     env.reset()
     for t in range(num_steps_per_epoch):
 
-        v = 0.1
         w = 0
 
         goal_pos = [w]
